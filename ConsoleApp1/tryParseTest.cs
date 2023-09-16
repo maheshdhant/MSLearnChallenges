4e80cd48501838ad0fb8dbd65e4a34e436e4a0b6
madmdx07 <mahessdhant@gmail.com>
2023-09-16 20:43:32 +0545

    Tested TryParse() in datatypes conversion.

---
+++ ConsoleApp1/tryParseTest.cs
@@ -0,0 +1,13 @@
+﻿string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+float valueHolder;
+float total = 0.0f;
+string message = "";
+foreach (var item in values){
+    if (float.TryParse(item, out valueHolder)){
+        total += valueHolder;
+    }
+    else{
+        message += item; 
+    }
+}
+Console.WriteLine(total + "\n" + message);
