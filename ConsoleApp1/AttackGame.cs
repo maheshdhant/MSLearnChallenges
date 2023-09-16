fb825e0f40058b64e110c3e3d94b449126ddbd4b
madmdx07 <mahessdhant@gmail.com>
2023-09-16 12:06:01 +0545

    Attack game challenge

--- ConsoleApp1/AttackGame.cs
+++ ConsoleApp1/AttackGame.cs
@@ -1,18 +1,26 @@
 using System.Runtime.InteropServices;
-int heroLife = 10;
-int monsterLife = 10;
-Random num = new Random();
-do{
 
-    int heroAttack = num.Next(1, 10);
-    int monsterAttack = num.Next(1,10);
+public class Challenges{
+    public int heroLife = 10;
+    public int monsterLife = 10;
+    Random num = new Random();
+    public void Fight(){
+        do{
+            int heroAttack = num.Next(1, 10);
+            int monsterAttack = num.Next(1,10);
 
-    monsterLife -= heroAttack;
-    Console.WriteLine("Hero attacked by: " + heroAttack+ " monster health: " + monsterLife);
-    if (monsterLife <= 0) continue;
+            monsterLife -= heroAttack;
+            Console.WriteLine("Hero attacked by: " + heroAttack+ " monster health: " + monsterLife);
+            if (monsterLife <= 0) continue;
 
-    heroLife -= monsterAttack;
-    Console.WriteLine("Monster attacked by: "+monsterAttack+ " hero health: " + heroLife);
+            heroLife -= monsterAttack;
+            Console.WriteLine("Monster attacked by: "+monsterAttack+ " hero health: " + heroLife);
 
-}while(heroLife>0 && monsterLife>0);
-Console.WriteLine(heroLife>monsterLife? "Hero won": "monster won");
+        }while(heroLife>0 && monsterLife>0);
+    
+        Console.WriteLine(heroLife>monsterLife? "Hero won": "monster won");
+    }
+    public void Main(){
+        Fight();
+    }
+}
