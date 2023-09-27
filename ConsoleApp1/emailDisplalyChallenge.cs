eb9fb814b22b5ce67ce2cbe5df8c26f6c6c96ec3
madmdx07 <mahessdhant@gmail.com>
2023-09-27 22:25:06 +0545

    Challenge to generate email address.

---
+++ ConsoleApp1/emailDisplalyChallenge.cs
@@ -0,0 +1,42 @@
+string firstName = "Mahesh";
+string lastName = "Dhant";
+string emailTail = "contoso.com";
+
+emailGenerator(firstName,lastName,emailTail);
+
+void emailGenerator(string firstName, string lastName, string emailTail){
+    string emailHead = firstName.Substring(0, 2).ToLower() + lastName.ToLower();
+    string email = emailHead + "@" + emailTail;
+    Console.WriteLine(email);
+}
+
+string[,] corporate = 
+{
+    {"Robert", "Bavin"}, {"Simon", "Bright"},
+    {"Kim", "Sinclair"}, {"Aashrita", "Kamath"},
+    {"Sarah", "Delucchi"}, {"Sinan", "Ali"}
+};
+
+string corporateDomain = "contoso.com";
+
+string[,] external = 
+{
+    {"Vinnie", "Ashton"}, {"Cody", "Dysart"},
+    {"Shay", "Lawrence"}, {"Daren", "Valdes"}
+};
+
+string externalDomain = "hayworth.com";
+
+for (int i = 0; i < corporate.GetLength(0); i++) 
+{
+    string emailHead = corporate[i,0].Substring(0, 2).ToLower() + corporate[i,1].ToLower();
+    string email = emailHead + "@" + corporateDomain;
+    Console.WriteLine(email);
+}
+
+for (int i = 0; i < external.GetLength(0); i++) 
+{
+    string emailHead = external[i,0].Substring(0, 2).ToLower() + external[i,1].ToLower();
+    string email = emailHead + "@" + externalDomain;
+    Console.WriteLine(email);
+}
