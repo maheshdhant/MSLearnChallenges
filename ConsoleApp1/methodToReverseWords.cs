e02b060ee9fa09fd95a94e6184280754611ff33f
madmdx07 <mahessdhant@gmail.com>
2023-09-28 18:40:25 +0545

    Removed few unnecessary lines.

--- ConsoleApp1/methodToReverseWords.cs
+++ ConsoleApp1/methodToReverseWords.cs
@@ -1,10 +1,13 @@
 string sentence = "Hello! My name is Shyam Kumar Nath.";
 string[] sentenceArray = sentence.Split(' ');
-Console.WriteLine(sentenceArray.Length);
 string reversedSentence = "";
+
 for ( int i=0; i<sentenceArray.Length; i++){
     reversedSentence += ReverseWord(sentenceArray[i]) + " ";
 }
+
+Console.WriteLine(reversedSentence);
+
 string ReverseWord(string word){
     string reversedWord = "";
     for (int i = word.Length - 1; i >= 0; i--){
@@ -12,4 +15,3 @@ string ReverseWord(string word){
     }
     return reversedWord;
 }
-Console.WriteLine(reversedSentence);
