6d42a8fb4bb849a73b126d5870b126e6fc99e00e
madmdx07 <mahessdhant@gmail.com>
2023-09-29 23:10:33 +0545

    RandomizeAnimals method added.

---
+++ ConsoleApp1/ContosoZooVisit.cs
@@ -0,0 +1,24 @@
+using System;
+
+string[] pettingZoo = 
+{
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese", 
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws", 
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+};
+
+//////////////////////     Methods      //////////////////
+
+void RandomizeAnimals()
+{
+    Random random = new Random();
+
+    for (int i = 0; i < pettingZoo.Length; i++) 
+    {
+        int r = random.Next(i, pettingZoo.Length);
+
+        string temp = pettingZoo[i];
+        pettingZoo[i] = pettingZoo[r];
+        pettingZoo[r] = temp;
+    }
+}
