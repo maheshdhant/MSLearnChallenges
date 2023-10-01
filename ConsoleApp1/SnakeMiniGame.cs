5503b7e94d7a1c587544faa8134159f37bec1519
madmdx07 <mahessdhant@gmail.com>
2023-10-01 23:35:17 +0545

    Small build up.

--- ConsoleApp1/SnakeMiniGame.cs
+++ ConsoleApp1/SnakeMiniGame.cs
@@ -1,23 +1,51 @@
 using System;
-
+Console.CursorVisible = false;
 // IsFoodConsumed();
 // UpdatePlayerStatus();
 // PauseMovementSpeed();
 // NewFoodLocation();
 // CharacterTerminateGame();
 
+Random random = new Random();
+
+// Terminal size
 var width = Console.WindowWidth;
 var height = Console.WindowHeight;
 
-TerminalTerminateGame(width, height);
+// Player
+var player = "@@@@@@";
+int playerX = 2;
+int playerY = 2;
+
+// Food
+var food = "*";
+
+// Set terminal
+bool terminalOn = true;
+
+//Start game
+while(terminalOn){
+    Console.Clear();
+    Position();
+    TerminalTerminateGame(width, height);
+}
+
+//Player and food position
+void Position(){
+    Console.SetCursorPosition(playerX, playerY);   // Player
+    Console.Write(player);
+
+    Console.SetCursorPosition(random.Next(0, width-5), random.Next(0, height-5));  // Food
+    Console.Write(food);
+    Thread.Sleep(1000);
+}
 
 void TerminalTerminateGame(int width, int height) 
 {
-
     Console.Clear();
-    Console.SetCursorPosition(5, 5);
     if (Console.WindowHeight != height && Console.WindowWidth != width){
         Console.Clear();
         Console.WriteLine("Console was resized. Program exiting."+width+ " "+ height);
+        terminalOn = false;
     }
 }
