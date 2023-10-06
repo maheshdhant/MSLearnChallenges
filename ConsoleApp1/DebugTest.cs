c733e7af5435fe46ecb908e34b6a9e3b2b351664
madmdx07 <mahessdhant@gmail.com>
2023-10-06 23:04:52 +0545

    Loads products.

--- ConsoleApp1/DebugTest.cs
+++ ConsoleApp1/DebugTest.cs
@@ -1,4 +1,22 @@
+// Loads products
+int productCount = 2000;
+string[,] products = new string[productCount, 2];
 
+LoadProducts(products, productCount);
+
+for (int i = 0; i < productCount; i++)
+{
+    string result;
+    result = Process1(products, i);
+
+    if (result != "obsolete")
+    {
+        result = Process2(products, i);
+    }
+}
+
+bool pauseCode = true;
+while (pauseCode == true) ;
 
 // Generates data
 
