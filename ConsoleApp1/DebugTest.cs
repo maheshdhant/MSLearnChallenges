f087aec93ae882741cbd178d7ae4720619c35d63
madmdx07 <mahessdhant@gmail.com>
2023-10-06 23:02:35 +0545

    Simulates data processing.

--- ConsoleApp1/DebugTest.cs
+++ ConsoleApp1/DebugTest.cs
@@ -31,3 +31,26 @@ static void LoadProducts(string[,] products, int productCount)
         products[i, 0] = prodID;
     }
 }
+
+// Simulates data processing
+
+static string Process1(string[,] products, int item)
+{
+    Console.WriteLine($"Process1 message - working on {products[item, 1]} product");
+
+    return products[item, 1];
+}
+
+static string Process2(string[,] products, int item)
+{
+    Console.WriteLine($"Process2 message - working on product ID #: {products[item, 0]}");
+    if (products[item, 1] == "new")
+        Process3(products, item);
+
+    return "continue";
+}
+
+static void Process3(string[,] products, int item)
+{
+    Console.WriteLine($"Process3 message - processing product information for 'new' product");
+}
