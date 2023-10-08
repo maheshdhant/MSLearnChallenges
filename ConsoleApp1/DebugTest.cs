cdfc9e94350d4ae39aedacb592881aa0fe4d5ac7
madmdx07 <mahessdhant@gmail.com>
2023-10-08 21:40:33 +0545

    Debugging in loop.

--- ConsoleApp1/DebugTest.cs
+++ ConsoleApp1/DebugTest.cs
@@ -1,45 +1,11 @@
-string? readResult;
-int startIndex = 0;
-bool goodEntry = false;
+bool exit = false;
+var rand = new Random();
+int num1 = 5;
+int num2 = 5;
 
-int[] numbers = { 1, 2, 3, 4, 5 };
-
-// Display the array to the console.
-Console.Clear();
-Console.Write("\n\rThe 'numbers' array contains: { ");
-foreach (int number in numbers)
+do
 {
-    Console.Write($"{number} ");
-}
+    num1 = rand.Next(1, 11);
+    num2 = num1 + rand.Next(1, 51);  //test condition in WATCH => num2/num1 > 5
 
-// To calculate a sum of array elements, 
-//  prompt the user for the starting element number.
-Console.WriteLine($"}}\n\r\n\rTo sum values 'n' through 5, enter a value for 'n':");
-while (goodEntry == false)
-{
-    readResult = Console.ReadLine();
-    goodEntry = int.TryParse(readResult, out startIndex);
-
-    if (startIndex > 5)
-    {
-        goodEntry = false;
-        Console.WriteLine("\n\rEnter an integer value between 1 and 5");
-    }
-}
-
-// Display the sum and then pause.
-Console.WriteLine($"\n\rThe sum of numbers {startIndex} through {numbers.Length} is: {SumValues(numbers, startIndex)}");
-
-Console.WriteLine("press Enter to exit");
-readResult = Console.ReadLine();
-
-// This method returns the sum of elements n through 5
-static int SumValues(int[] numbers, int n)
-{
-    int sum = 0;
-    for (int i = n; i < numbers.Length; i++)
-    {
-        sum += numbers[i];
-    }
-    return sum;
-}
+} while (exit == false);  // set exit[bool]'s value to true to exit the debugging
