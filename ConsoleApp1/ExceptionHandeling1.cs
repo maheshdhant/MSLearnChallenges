296e706110fab5f18c95f29e2944c6d9d483f48d
madmdx07 <mahessdhant@gmail.com>
2023-10-11 23:01:17 +0545

    Exception handeling; actually.

---
+++ ConsoleApp1/ExceptionHandeling1.cs
@@ -0,0 +1,14 @@
+double float1 = 3000.0;
+double float2 = 0.0;
+int number1 = 3000;
+int number2 = 0;
+
+try
+{
+    Console.WriteLine(float1 / float2);
+    Console.WriteLine(number1 / number2);
+}catch{
+    Console.WriteLine("An exception has been caught, most probably; divide by zero exception!");
+}
+
+Console.WriteLine("Exit program");
