a7e53998d5f748b05692ff2c4e09da8ea26e1397
madmdx07 <mahessdhant@gmail.com>
2023-10-14 23:22:50 +0545

    Rethrowing exceptions.

--- ConsoleApp1/CustomException.cs
+++ ConsoleApp1/CustomException.cs
@@ -8,14 +8,86 @@ foreach (string[] userEntries in userEnteredValues)
 {
     try
     {
-        BusinessProcess1(userEntries);
+        BusinessProcess(userEntries);
     }
     catch (Exception ex)
     {
-        if (ex.StackTrace.Contains("BusinessProcess1") && (ex is FormatException))
+        if (ex.StackTrace.Contains("BusinessProcess") && (ex is FormatException))
         {
             Console.WriteLine(ex.Message);
         }
+    }
+}
+
+static void BusinessProcess(String[] userEntries)
+{
+    int valueEntered;
+
+    foreach (string userValue in userEntries)
+    {
+        try
+        {
+            valueEntered = int.Parse(userValue);
+        }
+        catch (FormatException)
+        {
+            FormatException invalidFormatException = new FormatException("FormatException: User input values in 'BusinessProcess' must be valid integers");
+            throw invalidFormatException;
+        }
+    }
+}
+
+//////////////////////////// Re-throwing Exception /////////////////////////////
+
+try
+{
+    OperatingProcedure1();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Exiting application.");
+}
+
+static void OperatingProcedure1()
+{
+    string[][] userEnteredValues = new string[][]
+    {
+        new string[] { "1", "two", "3"},
+        new string[] { "0", "1", "2"}
+    };
+
+    foreach(string[] userEntries in userEnteredValues)
+    {
+        try
+        {
+            BusinessProcess1(userEntries);
+        }
+        catch (Exception ex)
+        {
+            if (ex.StackTrace.Contains("BusinessProcess1"))
+            {
+                if (ex is FormatException)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Corrective action taken in OperatingProcedure1");
+                }
+                else if (ex is DivideByZeroException)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Partial correction in OperatingProcedure1 - further action required");
+
+                    // re-throw the original exception
+                    throw;
+                }
+                else
+                {
+                    // create a new exception object that wraps the original exception
+                    throw new ApplicationException("An error occurred - ", ex);
+                }
+            }
+        }
+
     }
 }
 
@@ -28,11 +100,22 @@ static void BusinessProcess1(String[] userEntries)
         try
         {
             valueEntered = int.Parse(userValue);
+
+            checked
+            {
+                int calculatedValue = 4 / valueEntered;
+            }
         }
         catch (FormatException)
         {
             FormatException invalidFormatException = new FormatException("FormatException: User input values in 'BusinessProcess1' must be valid integers");
             throw invalidFormatException;
+        }
+        catch (DivideByZeroException)
+        {
+            DivideByZeroException unexpectedDivideByZeroException = new DivideByZeroException("DivideByZeroException: Calculation in 'BusinessProcess1' encountered an unexpected divide by zero");
+            throw unexpectedDivideByZeroException;
+
         }
     }
 }
