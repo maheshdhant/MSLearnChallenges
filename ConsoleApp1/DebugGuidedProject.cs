104ee70e5fbf6d539f9972521526ca6bbb25742a
madmdx07 <mahessdhant@gmail.com>
2023-10-15 22:16:48 +0545

    Updating methods.

--- ConsoleApp1/DebugGuidedProject.cs
+++ ConsoleApp1/DebugGuidedProject.cs
@@ -92,3 +92,81 @@ do
     readResult = Console.ReadLine();
 
 } while (readResult == null);
+
+static void LoadTillEachMorning(int[,] registerDailyStartingCash, int[] cashTill)
+{
+    cashTill[0] = registerDailyStartingCash[0, 1];
+    cashTill[1] = registerDailyStartingCash[1, 1];
+    cashTill[2] = registerDailyStartingCash[2, 1];
+    cashTill[3] = registerDailyStartingCash[3, 1];
+}
+
+static string MakeChange(int cost, int[] cashTill, int twenties, int tens = 0, int fives = 0, int ones = 0)
+{
+    string transactionMessage = "";
+
+    cashTill[3] += twenties;
+    cashTill[2] += tens;
+    cashTill[1] += fives;
+    cashTill[0] += ones;
+
+    int amountPaid = twenties * 20 + tens * 10 + fives * 5 + ones;
+    int changeNeeded = amountPaid - cost;
+
+    if (changeNeeded < 0)
+        transactionMessage = "Not enough money provided.";
+
+    Console.WriteLine("Cashier Returns:");
+
+    while ((changeNeeded > 19) && (cashTill[3] > 0))
+    {
+        cashTill[3]--;
+        changeNeeded -= 20;
+        Console.WriteLine("\t A twenty");
+    }
+
+    while ((changeNeeded > 9) && (cashTill[2] > 0))
+    {
+        cashTill[2]--;
+        changeNeeded -= 10;
+        Console.WriteLine("\t A ten");
+    }
+
+    while ((changeNeeded > 4) && (cashTill[1] > 0))
+    {
+        cashTill[2]--;
+        changeNeeded -= 5;
+        Console.WriteLine("\t A five");
+    }
+
+    while ((changeNeeded > 0) && (cashTill[0] > 0))
+    {
+        cashTill[0]--;
+        changeNeeded--;
+        Console.WriteLine("\t A one");
+    }
+
+    if (changeNeeded > 0)
+        transactionMessage = "Can't make change. Do you have anything smaller?";
+
+    if (transactionMessage == "")
+        transactionMessage = "transaction succeeded";
+
+    return transactionMessage;
+}
+
+static void LogTillStatus(int[] cashTill)
+{
+    Console.WriteLine("The till currently has:");
+    Console.WriteLine($"{cashTill[3] * 20} in twenties");
+    Console.WriteLine($"{cashTill[2] * 10} in tens");
+    Console.WriteLine($"{cashTill[1] * 5} in fives");
+    Console.WriteLine($"{cashTill[0]} in ones");
+    Console.WriteLine();
+}
+
+static string TillAmountSummary(int[] cashTill)
+{
+    return $"The till has {cashTill[3] * 20 + cashTill[2] * 10 + cashTill[1] * 5 + cashTill[0]} dollars";
+
+}
